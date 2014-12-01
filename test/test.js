dc4cd709c886dfc4c619e12be6160389be118221
Saiyan <saiyan@cssai.eu>
2014-12-01 15:54:15 +0100

    add simple unit-test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+
+var assert = require('assert');
+
+describe('Testing starts', function () {
+    it('should return "true"', function (done) {
+        assert.ok(true);
+        done();
+    });
+});
